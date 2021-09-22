a39c4a4670b249a44cac913cdff8393cc2220a75
Saam Koukpari <saam.koukpari@gmail.com>
2021-09-22 14:32:26 -0700

    implement connectivity message

---
+++ client.js
@@ -0,0 +1,31 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: 50542
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+  });
+
+  conn.on('connect', () => {
+    conn.write('Name: BLU');
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = {connect};
