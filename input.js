73f915a654d6d85433ec6bbb2bb720fca70904cf
Saam Koukpari <saam.koukpari@gmail.com>
2021-09-22 16:54:34 -0700

    added movement

--- input.js
+++ input.js
@@ -1,17 +1,30 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput);
+  stdin.on("data", (key) => {handleUserInput(key, conn)});
   return stdin;
 };
 
-const handleUserInput = function (key) {
+const handleUserInput = function (key, conn) {
   if (key === '\u0003') {
   process.exit()};  
+  if (key === "w") {
+    conn.write("Move: up");
+  }
+  if (key === "a") {
+    conn.write("Move: left");
+  }
+  if (key === "s") {
+    conn.write("Move: down");
+  }
+  if (key === "d") {
+    conn.write("Move: right");
+  }
 };
-
-setupInput();
 
 module.exports = {setupInput};
--- play.js
+++ play.js
@@ -1,3 +1,4 @@
 const {connect} = require("./client");
 const {setupInput} = require("./input");
-
+const conn = connect()
+setupInput(conn);
